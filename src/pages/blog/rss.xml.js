8cf95c19d54dd2c15db97a165e7baf0df11658e4
Riku Yoshikata/吉形 凌空 <riku.yoshikata@yoshikata-home.org>
2026-01-23 19:57:24 +0900

    RSS修正4

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -1,29 +1,24 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const blog = await getCollection('blog');
-  
-  // 日付でソート（新しい順）
-  const sortedPosts = blog.sort((a, b) => 
-    new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
-  );
+  const sortedPosts = blog.sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime());
 
   return rss({
-    title: 'りーろぐ',
+    title: 'RadioStrike',
     description: 'ITインフラ、サーバー、開発に関する技術ブログ',
     site: context.site,
-    items: sortedPosts.map((post) => {
-      // 本文がない場合の安全策
-      const bodyText = post.body ? post.body.substring(0, 160).replace(/[#*`]/g, '') : "";
-      
-      return {
-        title: post.data.title,
-        pubDate: new Date(post.data.date),
-        description: post.data.description || bodyText + '...',
-        link: `/blog/${post.slug}/`,
-      };
-    }),
-    customData: `<language>ja-jp</language>`,
+    items: sortedPosts.map((post) => ({
+      title: post.data.title,
+      pubDate: new Date(post.data.date),
+      description: post.data.description || "",
+      link: `/blog/${post.slug}/`,
+      // 本文をHTMLに変換して全文配信
+      content: sanitizeHtml(parser.render(post.body || '')),
+    })),
   });
 }
