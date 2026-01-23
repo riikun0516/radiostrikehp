53193d08c3a29583726c2856ab22fdcd379805d1
Riku Yoshikata/吉形 凌空 <riku.yoshikata@yoshikata-home.org>
2026-01-23 19:53:47 +0900

    RSS修正3

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -4,22 +4,26 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const blog = await getCollection('blog');
   
-  // 新しい順にソート（日付が文字列でも動くように工夫）
+  // 日付でソート（新しい順）
   const sortedPosts = blog.sort((a, b) => 
     new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
   );
 
   return rss({
-    title: 'RadioStrike',
+    title: 'りーろぐ',
     description: 'ITインフラ、サーバー、開発に関する技術ブログ',
     site: context.site,
-    items: sortedPosts.map((post) => ({
-      title: post.data.title,
-      pubDate: new Date(post.data.date),
-      // descriptionが空なら本文の冒頭を少し入れる
-      description: post.data.description || post.body.substring(0, 160).replace(/[#*`]/g, '') + '...',
-      link: `/blog/${post.slug}/`,
-    })),
+    items: sortedPosts.map((post) => {
+      // 本文がない場合の安全策
+      const bodyText = post.body ? post.body.substring(0, 160).replace(/[#*`]/g, '') : "";
+      
+      return {
+        title: post.data.title,
+        pubDate: new Date(post.data.date),
+        description: post.data.description || bodyText + '...',
+        link: `/blog/${post.slug}/`,
+      };
+    }),
     customData: `<language>ja-jp</language>`,
   });
 }
